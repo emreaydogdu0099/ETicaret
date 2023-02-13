9d777099f671bfb58143b43cc3d2ebfa57ce8175
Emre Aydoğduoğlu <92211425+emreaydogdu0099@users.noreply.github.com>
2023-02-13 14:14:36 +0300

    ETicaret

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,36 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(p=>p.Name)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("Lütfen ürün adını giriniz.")
+                .MaximumLength(150)
+                .MinimumLength(5)
+                .WithMessage("Lütfen ürün adını 5 ile 150 karakter arasında giriniz.");
+            RuleFor(p => p.Stock)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Lütfen ürünün stok bilgisini giriniz.")
+                .Must(s => s >= 0)
+                .WithMessage("Lütfen stok bilgisini 0 veya daha büyük giriniz");
+            RuleFor(p => p.Price)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Lütfen ürünün fiyat bilgisini giriniz.")
+                .Must(p => p > 0)
+                .WithMessage("Lütfen fiyat bilgisini 0'dan büyük giriniz");
+        }
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,76 @@
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Application.RequestParameters;
+using ETicaretAPI.Application.ViewModels.Products;
+using ETicaretAPI.Domain;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
+        {
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery]Pagination pagination)
+        {
+            var totalCount = _productReadRepository.GetAll(false).Count();
+            var products = _productReadRepository.GetAll(false).Select(p => new
+            {
+                p.Id,
+                p.Name,
+                p.Stock,
+                p.Price,
+                p.CreatedDate,
+                p.UpdatedDate
+            }).Skip(pagination.Page * pagination.Size).Take(pagination.Size);
+            return Ok(new
+            {
+                totalCount,
+                products
+            });
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            return Ok(await _productReadRepository.GetByIdAsync(id, false));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(VM_Create_Product model)
+        {
+            await _productWriteRepository.AddAsync(new()
+            {
+                Name = model.Name,
+                Price = model.Price,
+                Stock = model.Stock
+            });
+            await _productWriteRepository.SaveAsync();
+            return StatusCode((int)HttpStatusCode.Created);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Put(VM_Update_Product model)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(model.Id);
+            product.Stock = model.Stock;
+            product.Name = model.Name;
+            product.Price = model.Price;
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            await _productWriteRepository.RemoveAsync(id);
+            await _productWriteRepository.SaveAsync();
+            return Ok();
+        }
+    }
+}
